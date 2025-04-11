a8264cac55eae737af11725b202e597dab6ced4b
Rishat <rishat.khalikov@yandex.ru>
2025-04-11 15:12:52 +0500

    Generated resource Category  for Filament

--- database/migrations/2025_04_10_134909_create_categories_table.php
+++ database/migrations/2025_04_10_134909_create_categories_table.php
@@ -13,16 +13,18 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->nullable();
             $table->string('slug')->unique(); //unique() - проверка на уникальность. должен быть уникальным
-            $table->integer('parent_id')->nullable(); 
-            $table->timestamps();
+            $table->integer('parent_id')->nullable();
+
 
             // рекомендации ИИ Алиса
             $table->foreign('parent_id') // указал внешний ключ
                 ->references('id') // с каким полем будет связан этот ключ
                 ->on('categories') // какой таблицы
                 ->onDelete('set null'); // какое значение будет, если подкатегория будет удалена
+
+            $table->timestamps();
         });
     }
 
