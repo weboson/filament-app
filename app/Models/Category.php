9f19c846aa438dc5e3efa7f165b09b21e868aebc
Rishat <rishat.khalikov@yandex.ru>
2025-04-11 14:30:11 +0500

    Прислушился к ИИ Яндекс: также изменил (не много) модель Category.php

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -9,14 +9,14 @@ class Category extends Model
     protected $fillable = ['name', 'slug', 'parent_id'];
 
     // метод будет вызываться для ПОДКАТЕГОРИИ (у которой будет родитель КАТЕГОРИЯ)
-    public function subcategory()
+    public function children()
     {
-        return $this->hasMany(\App\Models\Category::class, 'parent_id'); // связь один ко многим (One to Many)
+        return $this->hasMany(Category::class, 'parent_id');
     }
 
     // для КАТЕГОРИИ (родителя)
     public function parent()
     {
-        return $this->belongsTo(\App\Models\Category::class, 'parent_id'); // связь 
+        return $this->belongsTo(Category::class);
     }
 }
