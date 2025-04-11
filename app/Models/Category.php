14744fb34e1cb98cdfdd13ff7d6e74d39aced9fe
Rishat <rishat.khalikov@yandex.ru>
2025-04-11 12:11:29 +0500

    Сделать бесконечно добавляемые категории со иерархическими связями: создал модель Category и миграцию

---
+++ app/Models/Category.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    protected $fillable = ['name', 'slug', 'parent_id'];
+
+    // метод будет вызываться для ПОДКАТЕГОРИИ (у которой будет родитель КАТЕГОРИЯ)
+    public function subcategory()
+    {
+        return $this->hasMany(\App\Models\Category::class, 'parent_id'); // связь один ко многим (One to Many)
+    }
+
+    // для КАТЕГОРИИ (родителя)
+    public function parent()
+    {
+        return $this->belongsTo(\App\Models\Category::class, 'parent_id'); // связь 
+    }
+}
